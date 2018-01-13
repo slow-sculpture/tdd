e2ce1b9f9046b398032165c7f15995d12a581056
ChristianR <christian.rogowski@yahoo.pl>
2018-01-13 15:55:34 +0100

    TDD

--- src/main/java/pl/sda/poznan/exercises/IsPrimeNumber.java
+++ src/main/java/pl/sda/poznan/exercises/IsPrimeNumber.java
@@ -4,7 +4,7 @@ public class IsPrimeNumber {
     public static boolean isPrime(int number) {
         // return false;
         boolean result = true;
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i <= number/2; i++) {
             if (number % i == 0) {
                 result = false;
                 break;
